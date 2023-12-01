4724d41f77f9c7e21d2bcd81a632a8b3d2a682c9
Jf120 <jafc120@gmail.com>
2023-11-30 21:51:23 -0600

    swagger functionality added

---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: "melodyhaven API",
+        description: "melodyhaven API Information and Documentation",
+        version: "1.0.0"
+    },
+    host: "localhost:3030",
+    schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
