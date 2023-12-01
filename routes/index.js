43268acc89087f9fdc4ffe88657f6b237c33c6ac
Jf120 <jafc120@gmail.com>
2023-11-30 21:23:07 -0600

    Project set up

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+// router.use('/', require('./swagger'));
+
+router.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports = router;    
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const mongodb = require('./data/database');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+  next();
+});
+app.use('/', require('./routes'));
+
+process.on('uncaughtException', (err, origin) => {
+  console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } 
+  else {
+    app.listen(port, () => {
+        console.log(`Database is listening and running on port ${port}`);
+      });
+    }
+});
+
